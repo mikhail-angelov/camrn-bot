d1c9531c78caad5ea5bc9a520a98f63d934b3e51
Mikhail.Angelov <>
2019-09-19 10:25:04 +0300

    fix hook url

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.TELEGRAM_TOKEN || 'any';
-const options = {
-  webHook: {
-    // Just use 443 directly
-    port: 443
-  }
-};
+const TelegramBot = require('node-telegram-bot-api')
+const token = process.env.TELEGRAM_TOKEN || 'any'
+const options = { webHook: { port: 443 } }
 
-const url = process.env.NOW_URL;
-console.log('--', process.env.BOT_URL, process.env.NOW_URL, token)
-const bot = new TelegramBot(token, options);
-bot.setWebHook(`${url}/bot${token}`);
+const url = process.env.BOT_URL || process.env.NOW_URL
+console.log('bot hook ur:', url)
+const bot = new TelegramBot(token, options)
+bot.setWebHook(`${url}/bot${token}`)
 
 bot.on('message', function onMessage(msg) {
-    console.log('-', msg)
-  bot.sendMessage(msg.chat.id, 'yo');
-});
+  console.log('-', msg)
+  bot.sendMessage(msg.chat.id, 'not ready yet')
+})
