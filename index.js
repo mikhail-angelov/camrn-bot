57d93928c77fd65e6d8cd026395dd771d60d7112
Mikhail.Angelov <>
2019-09-19 10:20:21 +0300

    fix deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const TOKEN = process.env.TELEGRAM_TOKEN || 'any';
 const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN || 'any';
 const options = {
   webHook: {
     // Just use 443 directly
@@ -7,11 +7,12 @@ const options = {
   }
 };
 
-const url = 'https://<your-domain>.now.sh' || process.env.BOT_URL;
-console.log('--', url, token)
-const bot = new TelegramBot(TOKEN, options);
-bot.setWebHook(`${url}/bot${TOKEN}`);
+const url = process.env.NOW_URL;
+console.log('--', process.env.BOT_URL, process.env.NOW_URL, token)
+const bot = new TelegramBot(token, options);
+bot.setWebHook(`${url}/bot${token}`);
 
 bot.on('message', function onMessage(msg) {
+    console.log('-', msg)
   bot.sendMessage(msg.chat.id, 'yo');
 });
